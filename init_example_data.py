454dda5cf92cf90551773bae82de3c657fa40d7d
leons-code-corner <leontursch@icloud.com>
2025-02-28 13:49:50 +0100

    updated example data

--- init_example_data.py
+++ init_example_data.py
@@ -9,14 +9,12 @@ def seed_database():
         
         # Admin und regulärer User erstellen
         admin = User(
-            username='admin', 
-            email='admin@example.com', 
+            username='admin',  
             password_hash=generate_password_hash('admin123'),
             is_admin=True
         )
         user = User(
             username='user', 
-            email='user@example.com', 
             password_hash=generate_password_hash('user123'),
             is_admin=False
         )
