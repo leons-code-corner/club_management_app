7f840c9807362ee841d61ba26d3f5c474ed8ee05
leons-code-corner <leontursch@icloud.com>
2025-02-28 14:06:32 +0100

    datetime objects created

--- init_example_data.py
+++ init_example_data.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from app.models import User, Member, MembershipType, Membership
 from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 def seed_database():
     app = create_app()
@@ -28,17 +29,17 @@ def seed_database():
         
         # Beispiel-Mitglieder erstellen
         members = [
-            Member(id = 1, name='Max Mustermann', email='max@example.com',phone = 123456, join_date = '2021-01-01'),
-            Member(id = 2, name='Erika Musterfrau', email='erika@example.com',phone = 234567, join_date = '2022-01-01'),
-            Member(id = 3, name='Hans Beispiel', email='hans@example.com', phone = 345678, join_date = '2020-01-01')
+            Member(id = 1, name='Max Mustermann', email='max@example.com',phone = 123456, join_date = datetime(2021, 1, 1)),
+            Member(id = 2, name='Erika Musterfrau', email='erika@example.com',phone = 234567, join_date = datetime(2022, 1, 1)),
+            Member(id = 3, name='Hans Beispiel', email='hans@example.com', phone = 345678, join_date = datetime(2020, 1, 1))
         ]
 
         # Beispiel-Mitgliedschaften erstellen
         memberships = [
-            Membership(id = 1, membership_start = '2021-01-01', fk_member = 1, fk_membership_type = 3),
-            Membership(id = 2, membership_start = '2022-01-01', fk_member = 2, fk_membership_type = 1),
-            Membership(id = 3, membership_start = '2020-01-01', fk_member = 3, membership_end = '2022-12-31', fk_membership_type = 1),
-            Membership(id = 4, membership_start = '2023-01-01', fk_member = 1, fk_membership_type = 2)
+            Membership(id = 1, membership_start = datetime(2021, 1, 1), fk_member = 1, fk_membership_type = 3),
+            Membership(id = 2, membership_start = datetime(2022, 1, 1), fk_member = 2, fk_membership_type = 1),
+            Membership(id = 3, membership_start = datetime(2020, 1, 1), fk_member = 3, membership_end = datetime(2022, 12, 31), fk_membership_type = 1),
+            Membership(id = 4, membership_start = datetime(2023, 1, 1), fk_member = 1, fk_membership_type = 2)
         ]
         
         db.session.add(admin)
